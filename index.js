ce586ccd6e3efd07c6385f66c2667c7ffead74d5
eliabn <30064396@tafe.wa.edu.au>
2023-08-13 21:15:58 +0930

    Create test question array and function to initialize app

--- index.js
+++ index.js
@@ -4,13 +4,29 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of your project?',
+    }
+]
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    // Filter through questions
+    .prompt(questions)
+    .then((answers) => {
+        // 
+        const readmeFileContent = generateMarkdown(answers);
+
+    // TODO: Create a function to write README file
+    fs.writeFile('index.md', readmeFileContent, (err) =>
+    err ? console.log(err) : console.log('Successfully created index.md!'));
+});
+}
+
 
 // Function call to initialize app
 init();
