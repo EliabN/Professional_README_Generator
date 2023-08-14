f0dbfc8b494e16beb6f1752f99dbdf6ff83cd73a
eliabn <30064396@tafe.wa.edu.au>
2023-08-14 23:16:55 +0930

    Created renderLicenseLink function

--- sampleREADME.md
+++ sampleREADME.md
@@ -1,10 +1,10 @@
-# Name Project Title
+# 
 
-![License](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)
+![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
 
 ## Description
 
-Create a profesional README.Md
+
 
 ## Table of Contents
 
@@ -20,37 +20,37 @@ Create a profesional README.Md
 
 ## Installation
 
-Npm (npm instal) and node index.Js to run
+
 
 ## Usage
 
-![Sample img](sample/sample-screenshot.png)
+
 
 ## Credits
 
-N/A
+
 
 ## License
 
-Mozilla Public License 2.0
+This project license is under the [Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0)
 
 ---
 
 ## Features
 
-Create a sapmleREADME.Md
+
 
 ## How to Contribute
 
-N/A
+
 
 ## Tests
 
-N/A
+
 
 ## Contact
-``````
+`````
 If you have a question can reach via
-Email: eliabefghhgf@gmail.com
-More of my work: [eliabN](https://github.com/eliabN/).
-``````
+Email: 
+More of my work: [](https://github.com//).
+`````
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  // Define a mapping of license types to their corresponding badge URLs
+  // Define a mapping of license types to their badge URLs
   const licenseBadgeUrls = {
     'MIT': 'https://img.shields.io/badge/License-MIT-yellow.svg',
     'Apache 2.0': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
     'Mozilla Public License 2.0': 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg',
     'GNU General Public License v3.0': 'https://img.shields.io/badge/License-GPL%20v3-blue.svg',
     'None': '',
-    // Add more license types and badge URLs as needed
   };
   // Check if the license type exists in the mapping
   if (licenseBadgeUrls.hasOwnProperty(license)) {
@@ -23,11 +22,39 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  // Define a mapping of license and license links
+  const licenseLinkUrls = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'Apache 2.0': 'https://www.apache.org/licenses/LICENSE-2.0',
+    'Mozilla Public License 2.0': 'https://www.mozilla.org/en-US/MPL/2.0/',
+    'GNU General Public License v3.0': 'https://www.gnu.org/licenses/gpl-3.0.en.html',
+    'None': '',
+  };
+  // Check if the license type exists in the mapping
+  if (licenseLinkUrls.hasOwnProperty(license)) {
+    // If the license type exists, return the corresponding license URL
+    return `[${license}](${licenseLinkUrls[license]})`;
+  } else {
+    // If the license type is not found, return an empty string
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license = 'None') {
+    return
+  } else {
+    // Call the renderLicenseLink function with the defined license type
+    const licLink = renderLicenseLink(questions.license);
+
+    return `## License
+
+    This project license is under the ${licLink}`
+  }
+}
 
 
 // TODO: Create a function to generate markdown for README
@@ -50,18 +77,20 @@ const senCaseContribute = toSentenceCase(questions.contribute);
 const senCaseTests = toSentenceCase(questions.tests);
 
 
-// Define the license type
-//const licenseType = 'MIT';
-
 // Call the renderLicenseBadge function with the defined license type
-const badge = renderLicenseBadge(questions.license);
+const licBadge = renderLicenseBadge(questions.license);
 
-// Print the badge
-console.log(badge);
+// Call the renderLicenseLink function with the defined license type
+const licLink = renderLicenseLink(questions.license);
 
-  return `# ${titleCaseTitle}
+// Call the renderLicenseLink function with the defined license type
+const licSection = renderLicenseLink(questions.license);
 
-${badge}
+
+
+return `# ${titleCaseTitle}
+
+${licBadge}
 
 ## Description
 
@@ -93,7 +122,7 @@ ${questions.credits}
 
 ## License
 
-${questions.license}
+This project license is under the ${licSection}
 
 ---
 
@@ -110,10 +139,11 @@ ${senCaseContribute}
 ${senCaseTests}
 
 ## Contact
-
+\`\`\`\`\`
 If you have a question can reach via
 Email: ${questions.email}
-More of my work: [${questions.github}](https://github.com/${questions.github}/).`;}
+More of my work: [${questions.github}](https://github.com/${questions.github}/).
+\`\`\`\`\``;}
 
 
 // Function to convert a string to title case
